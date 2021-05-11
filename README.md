957d85781c2f293104b2aae2d0596265c57060ef
Guillaume Leger <guillaumeleger140@gmail.com>
2021-05-11 15:31:35 +0200

    sécurité + README

---
+++ README.md
@@ -0,0 +1,10 @@
+So Pekocko API.
+
+pour lancer le frontend:
+_ Executer npm install dans le dossier.
+Puis demarrer avec "ng serve".
+(http://localhost:4200).
+
+pour lancer le backend:
+_ Executer npm install -g nodemon
+Puis lancer le serveur avec "nodemon server".
--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,25 @@ const mongoose = require("mongoose");
 const UserRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
 const path = require("path");
+const helmet = require("helmet");
+const mongoSanitize = require('express-mongo-sanitize');
 
+require('dotenv').config();
 mongoose
-  .connect(
-    "mongodb+srv://gui31:159357@cluster0.p2nfc.mongodb.net/cluster0?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.DB_URI,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true 
+    })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
+app.use(helmet());
+app.use(mongoSanitize());
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", 'http://localhost:4200');
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
